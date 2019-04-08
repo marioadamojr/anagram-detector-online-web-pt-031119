645243651efd6765bddf3a1f8669f3319f64b44c
Mario Adamo Jr <marioadamojr@gmail.com>
2019-04-08 22:36:14 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(sentence)
+    sentence.select do |given_word|
+      (given_word.split("").sort) == (@word.split("").sort)
+      end
+      # binding.pry
+  end
+end
